8a720e6b9095bb832e36205c43125505b17593a2
testsnake <testsnake12@gmail.com>
2023-07-03 09:52:48 -0700

    Update player.js

--- structures/player.js
+++ structures/player.js
@@ -1,9 +1,11 @@
 class Player {
     matches = [];
+    enabledSongPacks = [];
+    elo;
 
     constructor(discordID, initElo) {
         this.discordID = discordID;
         this.elo = initElo || 1000;
     }
-    
+
 }
